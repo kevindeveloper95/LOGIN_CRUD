81a05f147b8c1724567ecfd4d2f9aa948b2b007c
Kevin Meneses Parga <84256746+kevindeveloper95@users.noreply.github.com>
2023-01-26 17:31:27 -0600

    Update db_crud.js

--- src/database/db_crud.js
+++ src/database/db_crud.js
@@ -5,10 +5,10 @@ const mysql = require('mysql')
 
 const db = mysql.createConnection({
     
-    host     : process.env.DB_HOST2,
-    user     : process.env.DB_USER2,
-    password : process.env.DB_PASS2,
-    database : process.env.DB_DATABASE2
+    host     : "localhost",
+    user     : "root",
+    password : "password",
+    database : "info_users"
   
 })
 
