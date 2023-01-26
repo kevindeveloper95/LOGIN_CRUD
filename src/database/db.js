4782097131a26771efc583429ddcea0f9576ea33
Kevin Meneses Parga <84256746+kevindeveloper95@users.noreply.github.com>
2023-01-26 17:32:35 -0600

    Update db.js

--- src/database/db.js
+++ src/database/db.js
@@ -2,10 +2,10 @@
 const mysql = require('mysql')
 const connection = mysql.createConnection({
 
-  host     : process.env.DB_HOST,
-  user     : process.env.DB_USER,
-  password : process.env.DB_PASS,
-  database : process.env.DB_DATABASE
+  host     : "localhost",
+  user     : "root",
+  password : "password",
+  database : "login"
 
 
 });
