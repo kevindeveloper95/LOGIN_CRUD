239d5de5324a8387caa710a475340080fb0d7e30
Kevin Meneses Parga <84256746+kevindeveloper95@users.noreply.github.com>
2023-01-26 18:35:57 -0600

    Update authController.js

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -64,13 +64,13 @@ exports.login = async (req,res) => {
                   })
              }else{
                const id = results[0].id
-               const token = jwt.sign({id:id}, process.env.JWT_SECRET,{
-                 expiresIn: process.env.JWT_TIEMPO_EXPIRA
+               const token = jwt.sign({id:id},"super_secret",{
+                 expiresIn: "7d"
                })
                console.log(token + user)
 
                const cookiesOption = {
-                 expires: new Date(Date.now()+process.env.JWT_COOKIE_EXPIRA  + 9999),
+                 expires: new Date(Date.now()+ "90"  + 9999),
                  httpOnly: true
                }
                res.cookie('jwt',token,cookiesOption)
@@ -96,7 +96,7 @@ exports.login = async (req,res) => {
 exports.isAuthenticated = async (req,res, next)=>{
   if (req.cookies.jwt) {
     try {
-      const decodificada = await promisify(jwt.verify)(req.cookies.jwt, process.env.JWT_SECRET)
+      const decodificada = await promisify(jwt.verify)(req.cookies.jwt, "super_secret")
       connection.query('SELECT * FROM users WHERE id = ?',[decodificada.id], (error, results) => {
         if (!results) {
           return next()
